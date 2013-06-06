4d1487d04aa868bf3d54378df20f4fcbc4eec995
David Dao <david-dao@gmx.de>
2013-06-06 15:42:28 +0200

    Initial Commit

---
+++ NewickParser/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  NewickParser
+//
+//  Created by David Dao on 06.06.13.
+//  Copyright (c) 2013 David Dao. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
